d4dcfe64ff2b1e42d51b6fa68728d2069c362f43
andre_pereira <andre.rodrigo@sweetmedia.com.br>
2020-11-27 15:15:36 -0300

    create call steps

---
+++ Assets/Scripts/RegisterController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterController : MonoBehaviour
+{
+    public GameObject step1;
+    public GameObject step2;
+
+   public void CallRegisterStep2()
+   {
+       step1.SetActive(false);
+       step2.SetActive(true);
+   }
+}
