8e7310ddd59d349712aac85d669efd0cceea0394
andre_pereira <andre.rodrigo@sweetmedia.com.br>
2020-11-30 16:05:52 -0300

    create firebase conection

---
+++ Assets/Scripts/CallStepsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallStepsController : MonoBehaviour
+{
+    public GameObject main;
+    public GameObject step1;
+    public GameObject step2;
+
+    public void CallRegisterStep1()
+    {
+        main.SetActive(false);
+        step1.SetActive(true);
+        step2.SetActive(false);
+    }
+
+   public void CallRegisterStep2()
+   {
+       step1.SetActive(false);
+       step2.SetActive(true);
+   }
+}
